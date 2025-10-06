01069df329aa955e09d7c2e6690b42173858fce2
Renion Dev <lubustarcraft@gmail.com>
2025-10-07 00:01:56 +0200

    feat: Add mob spawning in main scene

---
+++ Main.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+	// Don't forget to rebuild the project so the editor knows about the new export variable
+	
+	[Export]
+	public PackedScene MobScene { get; set; }
+
+	// We also specified this function name in PascalCase in the editor's connection window.
+	private void OnMobTimerTimeout()
+	{
+		// Create a new instance of the Mob scene.
+		Mob mob = MobScene.Instantiate<Mob>();
+		
+		// Chose a random location on the SpawnPath
+		// We store the reference to the SpawnLocation node.
+		var mobSpawnLocation = GetNode<PathFollow3D>("Path3D/PathFollow3D");
+		// And give it a random offset.
+		mobSpawnLocation.ProgressRatio = GD.Randf();
+
+		Vector3 playerPosition = GetNode<Player>("Player").Position;
+		mob.Initialize(mobSpawnLocation.Position, playerPosition);
+		
+		// Spawn the mob by adding it to the main scene.
+		AddChild(mob);
+
+	}
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
