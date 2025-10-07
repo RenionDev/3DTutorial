d98622adb389a15b6e94ade33776f2eb88be678a
Renion Dev <lubustarcraft@gmail.com>
2025-10-07 21:02:04 +0200

    feat: Add player death logic

--- Main.cs
+++ Main.cs
@@ -27,14 +27,10 @@ public partial class Main : Node
 		AddChild(mob);
 
 	}
-	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	// We also specified this function name in PascalCase in the editor's connection window.
+	public void OnPlayerHit()
 	{
+		GetNode<Timer>("MobTimer").Stop();
 	}
 }
